8546fd80679ef41871617cb8910ea45fdb8fb192
wminaung <minwin243@gmail.com>
2023-01-25 13:24:09 +0630

    not dry but work

--- position/css-position.js
+++ position/css-position.js
@@ -1,14 +1,55 @@
 const fruits = document.querySelectorAll(".fruit");
 // fruits.forEach((fruit) => {});
-const positionHandler = (pos, selectElement) => {
-  const userChoiceValue = topInput.value;
+const positionHandler = (pos, selectElement, userChoiceValue) => {
   const selectElementTag = document.getElementById(selectElement);
   selectElementTag.style[pos] = userChoiceValue + "px";
 };
 
 const topInput = document.getElementById("top");
+const rightInput = document.getElementById("right");
+const leftInput = document.getElementById("left");
+const bottomInput = document.getElementById("bottom");
+
+let ISTOP = false;
+let ISBOTTOM = false;
 topInput.addEventListener("change", () => {
-  positionHandler("top", "orange");
+  ISTOP = true;
+  if (ISTOP && ISBOTTOM) {
+    alert("You should not insert both top & bottom");
+    window.location.reload();
+  }
+
+  positionHandler("top", "orange", topInput.value);
 });
-// const orangeTag = document.getElementById("orange");
-// console.log((orangeTag.style.top = "30px"));
+
+bottomInput.addEventListener("change", () => {
+  ISBOTTOM = true;
+  if (ISTOP && ISBOTTOM) {
+    alert("You should not insert both top & bottom");
+    window.location.reload();
+  }
+
+  positionHandler("bottom", "orange", bottomInput.value);
+});
+
+let ISLEFT = false;
+let ISRIGHT = false;
+leftInput.addEventListener("change", () => {
+  ISLEFT = true;
+  if (ISLEFT && ISRIGHT) {
+    alert("You should not insert both left & right");
+    window.location.reload();
+  }
+  const selectElement = document.getElementById("orange");
+  positionHandler("left", "orange", leftInput.value);
+});
+
+rightInput.addEventListener("change", () => {
+  ISRIGHT = true;
+  if (ISLEFT && ISRIGHT) {
+    alert("You should not insert both left & right");
+    window.location.reload();
+  }
+  const selectElement = document.getElementById("orange");
+  positionHandler("right", "orange", rightInput.value);
+});
